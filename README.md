c75eaeeb14c46729d8082f7a32cffd778a4af3e6
VEGLOhouetchenouGABIN <houetchenougabinveglo@gmail.com>
2024-03-30 02:22:45 +0100

    Infinite scroll scraper of in-person and online hackothons from  https://devpost.com/hackathons this 2024/30/03

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # In-Person-And-Online-Hackathons-Infinite-scroll-scraped-with-scrapy-spider
 In Person And Online Hackathons Infinite scroll scraped with scrapy spider
+
+
+### PROCESS TO RUN THIS SCRAPY SPIDER
+
+##### Firstly be sure that you have scrapy installed . Otherwise install it with : pip install scrapy
+
+##### After that you can generate now a single spider with: scrapy genspider "name of your spider" "following by domains if you want to add it"
+
+##### Finally you can copy just the content of the quotestoscrapespider.py file into your file 
+
+##### Just run it now with : scrapy runspider filename
+
+##### Additionally , if you want to save the output to a csv file or others options , run this command: scrapy runspider "your spider file name".py -o "your desired csv file name".csv -t csv
+
+
+# COMMANDS RAN TO SCRAPE THE DATA:
+
+### scrapy genspider inpersonAndOnlineHackathons x
+
+### scrapy runspider inpersonAndOnlineHackathons.py -o hackathons.csv -t csv
+
+## THANK YOU FOR YOUR ATTENTION!!
---
+++ inpersonAndOnlineHackathons.py
@@ -0,0 +1,47 @@
+import scrapy
+
+
+base = "https://devpost.com/api/hackathons?page={}"
+
+
+class InpersonandonlinehackathonsSpider(scrapy.Spider):
+    name = "inpersonAndOnlineHackathons"
+    # allowed_domains = ["x"]
+    page=1
+    number_of_hackathons = 0
+    start_urls = [base.format(page)]
+
+    def parse(self, response):
+        data = response.json()
+        print(data['meta']['fuzzy'])
+        hackathons = data['hackathons']
+        
+        for item in hackathons:
+            self.number_of_hackathons += 1
+            yield {
+                "Title": item["title"],
+                "Displayed Location": item["displayed_location"],
+                "Open State": item['open_state'],
+                "Analytics Identifier": item["analytics_identifier"],
+                "Url": item['url'],
+                "Submission Period Dates": item['submission_period_dates'],
+                "Themes": item['themes'],
+                "Prize Amount" : item['prize_amount'],
+                "Registrations Count": item['registrations_count'],
+                "Featured": item['featured'],
+                "Organization Name": item['organization_name'],
+                "Winners Announced": item['winners_announced'],
+                "Submission Gallery Url": item['submission_gallery_url'],
+                "Start A Submission Url": item['start_a_submission_url'],
+                "Invite Only": item['invite_only'],
+                "Eligibility Requirement Invite Only Description": item['eligibility_requirement_invite_only_description']
+                
+            }
+            
+        self.page += 1
+            
+        if self.number_of_hackathons < data["meta"]["total_count"]:
+            
+            next_page_url = base.format(self.page)
+
+            yield scrapy.Request(next_page_url)
